86de8cbbe0813a0286d1f623f4a5133f2dd201ae
Ali Mansour <alimansour@Alis-MacBook-Pro.local>
2019-07-28 15:47:08 -0600

    sw install update

--- src/sw-template.js
+++ src/sw-template.js
@@ -15,42 +15,21 @@ if ("function" === typeof importScripts) {
     });
     // install service worker
 
-    self.addEventListener("install", function(e) {
-      e.waitUntil(
-        Promise.all([
-          caches.open(STATIC_CACHE_NAME),
-          caches.open(APP_CACHE_NAME),
-          self.skipWaiting()
-        ]).then(function(storage) {
-          var static_cache = storage[0];
-          var app_cache = storage[1];
-          return Promise.all([
-            static_cache.addAll(CACHE_STATIC),
-            app_cache.addAll(CACHE_APP)
-          ]);
-        })
-      );
-    });
+   self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+      caches.open(dataCacheName).then(function (cache) {
+          console.log('[Service Worker] Caching app shell');
+          return cache.addAll(filesToCache);
+      }).then(function(e){
+        return self.skipWaiting();
+      })
+    );
+});
 
-    self.addEventListener("activate", function(e) {
-      e.waitUntil(
-        Promise.all([
-          self.clients.claim(),
-          caches.keys().then(function(cacheNames) {
-            return Promise.all(
-              cacheNames.map(function(cacheName) {
-                if (
-                  cacheName !== APP_CACHE_NAME &&
-                  cacheName !== STATIC_CACHE_NAME
-                ) {
-                  console.log("deleting", cacheName);
-                  return caches.delete(cacheName);
-                }
-              })
-            );
-          })
-        ])
-      );
+    // active service worker
+    self.addEventListener("activate", evt => {
+      console.log("service worker has been actived");
     });
 
     self.addEventListener("fetch", evt => {
