f2ef6bc864e0d4732d43a17cc4fa787305d268c8
Ali Mansour <alimansour@Alis-MacBook-Pro.local>
2019-07-28 15:37:04 -0600

    update servicer workers to reflect updates

--- src/sw-template.js
+++ src/sw-template.js
@@ -15,13 +15,42 @@ if ("function" === typeof importScripts) {
     });
     // install service worker
 
-    self.addEventListener("install", evt => {
-      console.log("service worker installed");
+    self.addEventListener("install", function(e) {
+      e.waitUntil(
+        Promise.all([
+          caches.open(STATIC_CACHE_NAME),
+          caches.open(APP_CACHE_NAME),
+          self.skipWaiting()
+        ]).then(function(storage) {
+          var static_cache = storage[0];
+          var app_cache = storage[1];
+          return Promise.all([
+            static_cache.addAll(CACHE_STATIC),
+            app_cache.addAll(CACHE_APP)
+          ]);
+        })
+      );
     });
 
-    // active service worker
-    self.addEventListener("activate", evt => {
-      console.log("service worker has been actived");
+    self.addEventListener("activate", function(e) {
+      e.waitUntil(
+        Promise.all([
+          self.clients.claim(),
+          caches.keys().then(function(cacheNames) {
+            return Promise.all(
+              cacheNames.map(function(cacheName) {
+                if (
+                  cacheName !== APP_CACHE_NAME &&
+                  cacheName !== STATIC_CACHE_NAME
+                ) {
+                  console.log("deleting", cacheName);
+                  return caches.delete(cacheName);
+                }
+              })
+            );
+          })
+        ])
+      );
     });
 
     self.addEventListener("fetch", evt => {
